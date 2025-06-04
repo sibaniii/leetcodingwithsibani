cdb6565d82a9fd3e2f2c2265284bc9c1af5e7f8b
sibaniii <sswale217@gmail.com>
2025-06-04 20:29:31 +0530

    26. Remove duplicates from sorted array.py

--- 26. Remove duplicates from sorted array.py
+++ 26. Remove duplicates from sorted array.py
@@ -7,6 +7,7 @@ Output: 2, nums = [1,2,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores)."""
 
+# THE CODE WHICH I WROTE
 class Solution(object):
     def removeDuplicates(self, nums):
         k = len(nums)
