b0ef7f4b4517b64ff5834ff42f7d118c079e11b7
shivani wale <sswale217@gmail.com>
2025-08-22 17:19:31 +0530

    9. Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        a=str(x)
+        b=a[::-1]
+        if a==b:
+            return True
+        else:
+            return False
+        
