f4306d15e298a4acacb37d4f39a1ebf6c63b2291
shivani wale <sswale217@gmail.com>
2025-08-23 12:34:30 +0530

    69. Sqrt(x).py

---
+++ 69. Sqrt(x).py
@@ -0,0 +1,10 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        a=math.sqrt(x)
+        return int(a)
+        if a==float:
+            b=math.floor(a)
+            return int(b)
+        
+        
