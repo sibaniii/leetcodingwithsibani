93173b703e19da3bd84e28848f6cb0b21e9c021d
shivani wale <sswale217@gmail.com>
2025-08-25 19:28:20 +0530

    20. Valid Parentheses.py

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isValid(self, s):
+        while "()" in s or "[]" in s or "{}" in s:
+            s = s.replace("()", "")
+            s = s.replace("[]", "")
+            s = s.replace("{}", "")
+        
+        if s == "":
+            return True
+        else:
+            return False
